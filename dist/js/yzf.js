6d9137923240db21fb07ec370ce4c28b848f74dc
ch_sss <sdhhqb@gmail.com>
2017-06-07 10:57:22 +0800

    打包更新0607002

--- dist/js/yzf.js
+++ dist/js/yzf.js
@@ -1269,7 +1269,7 @@ var Modal = function ($) {
         }, {
             key: 'config',
             value: function config(_config4) {
-                this._config = $.extends({}, this._config, _config4);
+                this._config = $.extend({}, this._config, _config4);
             }
         }, {
             key: 'hide',
@@ -1280,13 +1280,13 @@ var Modal = function ($) {
             key: 'ok',
             value: function ok() {
                 this.hide();
-                this._config['succ'] && this._config['succ']();
+                this._config['confirm'] && this._config['confirm'](this._element);
             }
         }, {
             key: 'cancel',
             value: function cancel() {
                 this.hide();
-                this._config['fail'] && this._config['fail']();
+                this._config['cancel'] && this._config['cancel'](this._element);
             }
         }], [{
             key: '_jQueryInterface',
@@ -1463,248 +1463,187 @@ var Navbara = function ($) {
     $.fn[NAME] = Navbara._jQueryInterface;
     return Navbara;
 }(jQuery);
-;
-(function ($, window, document, undefined) {
+//分页插件
+;(function ($) {
 
-    'use strict';
+    function Page(container, fn, args, style) {
+        //存参数
+        var container = this.container = container || null;
+        if (this.container.constructor != jQuery) {
+            throw 'page插件第一个参数错误，请录入jQuery对象';
+        }
+        this.fn = fn || function () {};
+        if (typeof this.fn != "function") {
+            throw 'page插件第二个参数错误，请录入函数类型对象';
+        }
+        //存 args里的属性
+        var args = this.args = $.extend({
+            pageCount: 10, //总页码,默认10
+            current: 1, //当前页码,默认1
+            showPrev: true, //是否显示上一页按钮
+            showNext: true, //是否显示下一页按钮
+            showTurn: true, //是否显示跳转,默认显示
+            showSumNum: true, //是否显示总页码
+            showNear: 2, //显示当前页码前多少页和后多少页，默认2
+            pageSwap: true, //是否同意调剂,默认是。调剂会最大数量显示页码。例如当前页码之前无页码可以显示。则会增加之后的页码。
+            align: 'right' //对齐方式。默认右对齐.可选：left,right
+        }, args || {});
 
-    var old = $.fn.yzfPagination;
+        var width = this.width = parseInt(this.container.css('width')) || parseInt(this.container.parent().css('width'));
+        var height = this.height = 20; //parseInt(this.obj.css('height'));
 
-    // PROTOTYPE AND CONSTRUCTOR
+        var style = this.style = $.extend({
+            "fontSize": 12, //字体大小
+            "width": width, //页码盒子总宽度
+            "height": height, //页码总高度，默认20px
+            "pagesMargin": 2, //每个页码或按钮之间的间隔
+            "paddL": 0, //左边留白
+            "paddR": 0, //右边留白
+            "prevNextWidth": 48, //上页下页的宽度
+            "pagecountWidth": 48, //共计多少页的宽度
+            "trunWidth": 110 //跳转模块宽度
+        }, style || {});
 
-    var YZFPagination = function YZFPagination(element, options) {
-        this.$element = $(element);
-        this.options = $.extend({}, $.fn.yzfPagination.defaults, options);
-        this.init(this.options);
+        if (!style.pageWidth) style.pageWidth = function () {
+            var sumWidth = style.width - (style.prevNextWidth + 2 + style.pagesMargin) * (Number(args.showPrev) + Number(args.showNext)) - (style.pagecountWidth + style.pagesMargin) * Number(args.showSumNum) - (style.trunWidth + style.pagesMargin) * Number(args.showTurn);
+            var sumLength = args.showNear * 2 + 5;
+            return parseInt(sumWidth / sumLength) - style.pagesMargin;
+        }(); //每个页码的宽度。默认按最大摆放量计算
+
+        //设置容器样式
+        this.container.css({ 'pading-left': style.paddL + 'px', 'pading-right': style.paddR + 'px', 'content': '', 'display': 'block', 'clear': 'both' });
+        //建立自己的容器
+        this.container.html('<div></div>');
+        this.obj = this.container.children();
+        this.obj.css({ 'content': '', 'display': 'block', 'clear': 'both' });
+        //初始化
+        this.init();
+    }
+
+    Page.prototype.init = function () {
+        this.fillHtml();
+        this.bindEvent();
     };
 
-    YZFPagination.prototype = {
+    //填充DOM
+    Page.prototype.fillHtml = function () {
+        var args = this.args;
+        var obj = this.obj;
+        if (args.current > args.pageCount || args.current < 1) return;
+        obj.empty();
 
-        constructor: YZFPagination,
-
-        init: function init(options) {
-            this.options = $.extend({}, this.options, options);
-            this.currentPages = this.getPages_v_1_1(this.options.startPage);
-
-            if (this.options.startPage < 1 || this.options.startPage > this.options.totalPages) {
-                throw new Error('Start page option is incorrect');
-            }
-
-            if (this.options.totalPages <= 0) {
-                throw new Error('Total pages option cannot be less 1 (one)!');
-            }
-
-            if (!$.isNumeric(this.options.visiblePages) && !this.options.visiblePages) {
-                this.options.visiblePages = this.options.totalPages;
-            }
-
-            if (this.options.onPageClick instanceof Function) {
-                this.$element.bind('page', this.options.onPageClick);
-            }
-
-            var tagName = typeof this.$element.prop === 'function' ? this.$element.prop('tagName') : this.$element.attr('tagName');
-
-            if (tagName === 'UL') {
-                this.$listContainer = this.$element;
+        //上一页
+        if (args.showPrev) {
+            if (args.current > 1) {
+                obj.append('<a href="javascript:;" class="prevPage"><上一页</a>');
             } else {
-                this.$listContainer = $('<ul></ul>');
+                obj.remove('.prevPage');
+                obj.append('<span class="disabled"><上一页</span>');
             }
-
-            this.$listContainer.addClass(this.options.paginationClass);
-
-            this.$listContainer.append(this.buildListItems(this.currentPages.numeric));
-
-            if (tagName !== 'UL') {
-                this.$element.append(this.$listContainer);
-            }
-
-            this.show(this.options.startPage);
-        },
-
-        show: function show(page) {
-            if (page < 1 || page > this.options.totalPages) {
-                throw new Error('Page is incorrect.');
-            }
-            this.render(this.getPages_v_1_1(page));
-
-            this.setupEvents();
-
-            this.$element.trigger('page', page);
-        },
-
-        buildListItems: function buildListItems(pages) {
-            var $listItems = $();
-
-            if (this.options.first) {
-                $listItems = $listItems.add(this.buildItem('first', 1));
-            }
-
-            if (this.options.prev) {
-                $listItems = $listItems.add(this.buildItem('prev', 1));
-            }
-
-            for (var i = 0; i < pages.length; i++) {
-                $listItems = $listItems.add(this.buildItem('page', pages[i]));
-            }
-
-            if (this.options.next) {
-                $listItems = $listItems.add(this.buildItem('next', 2));
-            }
-
-            if (this.options.last) {
-                $listItems = $listItems.add(this.buildItem('last', this.options.totalPages));
-            }
-
-            return $listItems;
-        },
-
-        buildItem: function buildItem(type, page) {
-            var itemContainer = $('<li></li>'),
-                itemContent = $('<a></a>'),
-                itemText = null;
-
-            itemContainer.addClass(type);
-            itemContainer.attr('data-page', page);
-
-            switch (type) {
-                case 'page':
-                    itemText = page;
-                    break;
-                case 'first':
-                    itemText = this.options.first;
-                    break;
-                case 'prev':
-                    itemText = this.options.prev;
-                    break;
-                case 'next':
-                    itemText = this.options.next;
-                    break;
-                case 'last':
-                    itemText = this.options.last;
-                    break;
-                default:
-                    break;
-            }
-
-            itemContainer.append(itemContent.attr('href', this.href(page)).text(itemText));
-            return itemContainer;
-        },
-
-        getPages_v_1_1: function getPages_v_1_1(currentPage) {
-            var pages = [];
-
-            var half = Math.floor(this.options.visiblePages / 2);
-            var start = currentPage - half + 1 - this.options.visiblePages % 2;
-            var end = currentPage + half;
-
-            // handle boundary case
-            if (start <= 0) {
-                start = 1;
-                end = this.options.visiblePages;
-            }
-            if (end > this.options.totalPages) {
-                start = this.options.totalPages - this.options.visiblePages + 1;
-                end = this.options.totalPages;
-            }
-
-            var itPage = start;
-            while (itPage <= end) {
-                pages.push(itPage);
-                itPage++;
-            }
-
-            return { "currentPage": currentPage, "numeric": pages };
-        },
-
-        render: function render(pages) {
-            if (!this.equals(this.currentPages.numeric, pages.numeric)) {
-                this.$listContainer.children().remove();
-                this.$listContainer.append(this.buildListItems(pages.numeric));
-                this.currentPages = pages;
-            }
-
-            this.$listContainer.find('.page').removeClass('active');
-            this.$listContainer.find('.page').filter('[data-page="' + pages.currentPage + '"]').addClass('active');
-
-            this.$listContainer.find('.first').toggleClass('disabled', pages.currentPage === 1);
-
-            this.$listContainer.find('.last').toggleClass('disabled', pages.currentPage === this.options.totalPages);
-
-            var prev = pages.currentPage - 1;
-            this.$listContainer.find('.prev').toggleClass('disabled', pages.currentPage === 1).data('page', prev > 1 ? prev : 1);
-
-            var next = pages.currentPage + 1;
-            this.$listContainer.find('.next').toggleClass('disabled', pages.currentPage === this.options.totalPages).data('page', next < this.options.totalPages ? next : this.options.totalPages);
-        },
-
-        setupEvents: function setupEvents() {
-            var base = this;
-            this.$listContainer.find('li').each(function () {
-                var $this = $(this);
-                $this.off();
-                if ($this.hasClass('disabled') || $this.hasClass('active')) return;
-                $this.click(function () {
-                    base.show(parseInt($this.data('page'), 10));
-                });
-            });
-        },
-
-        equals: function equals(oldArray, newArray) {
-            var i = 0;
-            while (i < oldArray.length || i < newArray.length) {
-                if (oldArray[i] !== newArray[i]) {
-                    return false;
-                }
-                i++;
-            }
-            return true;
-        },
-
-        href: function href(c) {
-            return this.options.href.replace(this.options.hrefVariable, c);
+        }
+        //中间页码
+        if (args.current != 1) {
+            obj.append('<a href="javascript:;" class="tcdNumber">' + 1 + '</a>');
+        } else {
+            obj.append('<span class="current">' + 1 + '</span>');
         }
 
+        if (args.current > args.showNear + 2) {
+            obj.append('<span class="hiding">...</span>');
+        }
+
+        var start = args.current > args.showNear + 2 ? args.current - args.showNear : 2,
+            end = args.current + args.showNear >= args.pageCount ? args.pageCount - 1 : args.current + args.showNear;
+
+        if (args.pageSwap) {
+            var dstart = args.current - args.showNear - 2;
+            var dend = args.pageCount - 1 - args.current - args.showNear;
+            if (dstart < 1 && dend > 1) {
+                end += Math.min(dend, Math.abs(dstart - 1));
+            } else if (dstart > 1 && dend < 1) {
+                start -= Math.min(dstart, Math.abs(dend - 1));
+            }
+        }
+
+        for (; start <= end; start++) {
+            if (start != args.current) {
+                obj.append('<a href="javascript:;" class="tcdNumber">' + start + '</a>');
+            } else {
+                obj.append('<span class="current">' + start + '</span>');
+            }
+        }
+
+        if (args.current + 1 + args.showNear < args.pageCount) {
+            obj.append('<span class="hiding">...</span>');
+        }
+
+        if (args.current != args.pageCount && args.pageCount != 1) {
+            obj.append('<a href="javascript:;" class="tcdNumber">' + args.pageCount + '</a>');
+        } else if (args.current == args.pageCount && args.pageCount != 1) {
+            obj.append('<span class="current">' + args.pageCount + '</span>');
+        }
+        //下一页
+        if (args.showNext) {
+            if (args.current == args.pageCount || args.pageCount == 1) {
+                obj.remove('.nextPage');
+                obj.append('<span class="disabled">下一页></span>');
+            } else {
+                obj.append('<a href="javascript:;" class="nextPage">下一页></a>');
+            }
+        }
+
+        if (args.showSumNum) {
+            obj.append('<span class="pagecount">共' + args.pageCount + '页</span>');
+        }
+        //跳转页码
+        if (args.showTurn) {
+            obj.append('<span class="countYe">到第<input type="text" maxlength=' + args.pageCount.toString().length + '>页<a href="javascript:;" class="turndown">确定</a></span>');
+        }
+        this.setStyle();
+        this.fn && this.fn(args.current);
     };
+    //添加样式
+    Page.prototype.setStyle = function () {};
 
-    // PLUGIN DEFINITION
+    //绑定事件
+    Page.prototype.bindEvent = function () {
+        var obj = this.obj;
+        var _this = this;
 
-    $.fn.yzfPagination = function (option) {
-        var args = Array.prototype.slice.call(arguments, 1);
-        var methodReturn;
-
-        var $set = this.each(function () {
-            var $this = $(this);
-            var data = $this.data('twbs-pagination');
-            var options = (typeof option === 'undefined' ? 'undefined' : _typeof(option)) === 'object' && option;
-
-            if (!data) $this.data('twbs-pagination', data = new YZFPagination(this, options));
-            if (typeof option === 'string') methodReturn = data[option].apply(data, args);
+        obj.off("click");
+        obj.on("click", "a.tcdNumber", function () {
+            _this.args.current = parseInt($(this).text());
+            _this.fillHtml();
         });
-
-        return methodReturn === undefined ? $set : methodReturn;
+        //上一页
+        obj.on("click", "a.prevPage", function () {
+            _this.args.current = parseInt(obj.children("span.current").text()) - 1;
+            _this.fillHtml();
+        });
+        //下一页
+        obj.on("click", "a.nextPage", function () {
+            _this.args.current = parseInt(obj.children("span.current").text()) + 1;
+            _this.fillHtml();
+        });
+        //跳转
+        obj.on("click", "a.turndown", function () {
+            var page = _this.args.current = Number(obj.children("span.countYe").children('input').val());
+            if (page > _this.args.pageCount) {
+                alert("页码输入有误，请重新输入！");
+                return;
+            }
+            _this.fillHtml();
+        });
     };
 
-    $.fn.yzfPagination.defaults = {
-        totalPages: 0,
-        startPage: 1,
-        visiblePages: 5,
-        href: 'javascript:void(0);',
-        hrefVariable: '{{number}}',
-        first: '首页',
-        prev: '上一页',
-        next: '下一页',
-        last: '尾页',
-        paginationClass: 'pagination',
-        onPageClick: null
-    };
-
-    $.fn.yzfPagination.Constructor = YZFPagination;
-
-    $.fn.yzfPagination.noConflict = function () {
-        $.fn.yzfPagination = old;
+    //绑定成jQuery插件
+    $.fn.createPage = function (fn, args, style) {
+        var _this = this;
+        new Page(_this, fn, args, style);
         return this;
     };
-})(jQuery, window, document);
+})(jQuery);
+
 /**
  * Created by xiening on 2017/5/19.
  */
